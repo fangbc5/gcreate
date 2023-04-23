49503c22e91b9f7d93f684a5fb1011263764ec5c
fangbchun <fangbchun@yonyou.com>
2023-04-23 09:59:52 +0800

    asdf

--- pongo/pongo.go
+++ pongo/pongo.go
@@ -1,15 +1,11 @@
 package templates
 
 import (
-	"encoding/json"
-	"gcreate/conf"
-	"gcreate/db"
-	"gcreate/handler"
-	"github.com/flosch/pongo2/v6"
-	"log"
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/flosch/pongo2/v6"
 )
 
 type Pongo struct {
@@ -20,34 +16,14 @@ func MakePongo() *Pongo {
 }
 
 func (p *Pongo) Exec(data interface{}) {
-	handler.Try(func() {
-		pongo2.RegisterFilter("substr", substr)
-		pongo2.RegisterFilter("totype", totype)
-		// 获取目录下的所有模版文件
-		getfiles(conf.MakeConfig().Tmpl, conf.MakeConfig().Out, func(srcFile string, destFile string) {
-			template, _ := pongo2.FromFile(srcFile)
-			//pongo2.ApplyFilter("firstWord", , pongo2.Value{})
-			//准备数据
-			tables := data.([]db.Table)
-			for _, table := range tables {
-				marshal, _ := json.Marshal(table)
-				context := pongo2.Context{}
-				err := json.Unmarshal(marshal, &context)
-				if err != nil {
-					log.Println(err)
-				}
-				//destFile名称处理
-				filename := getFileName(destFile, table.TableName)
-				file, _ := os.Create(filename)
-				err = template.ExecuteWriter(context, file)
-				if err != nil {
-					log.Println(err)
-				}
-			}
-		})
-	}, func(err interface{}) {
-		log.Panicln(err)
-	})
+
+	pongo2.RegisterFilter("substr", substr)
+	pongo2.RegisterFilter("totype", totype)
+	// template, _ := pongo2.FromFile(srcFile)
+	// err = template.ExecuteWriter(context, file)
+	// if err != nil {
+	// 	log.Println(err)
+	// }
 
 }
 
